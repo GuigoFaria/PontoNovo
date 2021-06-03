dfda7e4c735d2929a86b5333b4a3addf9257368e
Guilherme Faria <guilhermeraulino2704@gmail.com>
2021-06-02 21:02:55 -0300

    New empty project + connection mongoDB

---
+++ ponto-novo/src/main/java/com/novo/ponto/pontonovo/PontoNovoApplication.java
@@ -0,0 +1,13 @@
+package com.novo.ponto.pontonovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoNovoApplication.class, args);
+	}
+
+}
---
+++ ponto-novo/src/test/java/com/novo/ponto/pontonovo/PontoNovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novo.ponto.pontonovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoNovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
